eb2e91aed74a6d5d135f1aedea2f9bd1e7c485a2
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2026-04-24 09:49:11 +0530

    Profile Image Auto Deletion, Password Reset Mechanism
    Profile image auto deletion of old photos

--- 09_Password_Reset/blog_project/user/models.py
+++ 09_Password_Reset/blog_project/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
 # Create your models here.
 
 
@@ -9,3 +10,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def save(self, *args, **kwargs):
+        old_profile = Profile.objects.get(id = self.id)
+
+        if old_profile.image and old_profile.image != self.image:
+            if os.path.isfile(old_profile.image.path):
+                os.remove(old_profile.image.path)
+        
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+
+        super().delete(*args, **kwargs)
